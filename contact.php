47386e1b9747de3e48ce75c5f1507e3a9315e5e4
Alexis Brunet <albrunet2000@gmail.com>
2019-05-28 22:44:47 +0200

    Update website

--- contact.php
+++ contact.php
@@ -12,30 +12,61 @@
         <h1>Projet INSA Caméra</h1>
       </a>
       </div>
-      <div class="nav">
+   <!---   <div class="nav">
         <div class="nav1">
-          <a href="installation.html">Installation</a>
-        </div>
-        <div class="nav1">
-          <a href="documentation.html" >Documentation</a>
+          <a href="documentation.html">Documentation</a>
         </div>
         <div class="nav1">
           <a href="contact.php">Contact</a>
         </div>
         <div class="nav1">
           <a href="credit.html">Crédit</a>
-        </div>
+        </div>-->
         
       </div>
     </div>
 <hr>
     <div class="corps">
-    <h2>Contact</h2>
+      <div class="corpsg">
+        <h1>Contact</h1>
+
+      </div>
+      <div class="corpsd">
+        <h1>Sommaire</h1>
+        <ol>
+          <li><a href="index.html">Présentation du projet</a></li>
+          <li><a href="presentationmat.html">Présentation du matériel</a></li>
+          <li><a href="presentationpi.html">Présentation de la Raspberry Pi</a></li>
+              <ul>
+                <li><a href="presentationpi.html#cocam">Connexion de la caméra</a></li>
+                <li><a href="presentationpi.html#copir">Connexion du PIR HC-SR501</a></li>
+              </ul>
+          <li><a href="config.html">Configuration de la Raspberry Pi</a></li>
+          <li><a href="installation.html">Installation des dépendances</a></li>
+              <ul>
+                <li><a href="installation.html#opencv4">Installation d'OpenCV4</a></li>
+                <li><a href="installation.html#flask">Installation de Flask</a></li>
+                <li><a href="installation.html#picamera">Installation de PiCamera</a></li>
+                <li><a href="installation.html#imutils">Insatallation Imutils</a></li>
+                <li><a href="installation.html#flaskbasicauth">Installation de Flask-BasicAuth</a></li>
+              </ul>
+          <li><a href="personnalisation.html">Personnalisation du programme</a></li>
+          <li><a href="lancement.html">Lancement du programme</a></li>
+          <li><a href="realsup.html">Réalisations supplémentaires</a></li>
+              <ul>
+                <li><a href="realsup.html#realapp">Réalisation de l'application</a></li>
+                <li><a href="realsup.html#realsup">Réalisation du support</a></li>
+              </ul>
+          <li><a href="documents.html">Documents supplémentaires</a></li>
+        </ol>
+      </div>
     </div>
 <hr>
     <div class="pied">
+      <a href="contact.php">Contact</a><br>
       <a href="credit.html">©2019 - Crédit</a>
     </div>
   </body>
 </html>
+
 
